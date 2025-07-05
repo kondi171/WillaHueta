a48bf68234e645092241e2a85f48a67ac6ae94f8
kondi171 <wk.k.nowak@gmail.com>
2025-07-05 21:13:47 +0200

    villa history section finished

---
+++ client/src/components/Landing.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import Header from "./Header.vue";
+import Nav from "./Nav.vue";
+// import News from "./News.vue";
+import Footer from "./footer/Footer.vue";
+import VillaHistory from "./villa-history/VillaHistory.vue";
+</script>
+
+<template>
+  <div class="landing">
+    <div class="wrapper">
+      <Header />
+      <Nav />
+    </div>
+    <VillaHistory />
+    <Footer />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.landing {
+  background-image: url("@/assets/img/bg/so-white.png");
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+}
+</style>
